6f4770fd56d6fb231ed1dba922ff272ca227cee9
Sebastian Grabiński <sgrabinski@parasoft.com.pl>
2026-05-15 11:35:43 +0200

    update branch1

--- src/main/java/examples/eval/Simple.java
+++ src/main/java/examples/eval/Simple.java
@@ -19,7 +19,55 @@ public class Simple {
      * @param index
      * @return map for the index
      */
+	public static int map2(int index2) {
+        switch (index2) {
+        case 0:
+            case10: return -1;
+        case 2:
+        case 20:
+            break;
+        default:
+            return -2;
+        }
+        return 0;
+    }
     public static int map(int index) {
+        switch (index) {
+        case 0:
+            case10: return -1;
+        case 2:
+        case 20:
+            break;
+        default:
+            return -2;
+        }
+        return 0;
+    }
+	public static int map1(int index) {
+        switch (index) {
+        case 0:
+            case10: return -1;
+        case 2:
+        case 20:
+            break;
+        default:
+            return -2;
+        }
+        return 0;
+    }
+		public static int map1_1(int index) {
+        switch (index) {
+        case 0:
+            case10: return -1;
+        case 2:
+        case 20:
+            break;
+        default:
+            return -2;
+        }
+        return 0;
+    }
+			public static int map1_2(int index) {
         switch (index) {
         case 0:
             case10: return -1;
